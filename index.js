34b0830e472ed9a651617420f89265a1609875b4
Virginia Dooley <vcdooley@gmail.com>
2019-04-11 16:43:33 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+const companyName = "Scuber"
+
+let mostProfitableNeighborhood = "Upper West Side"
+
+mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo = "Lauren Hart"
+
+companyCeo = "Susan Smith"
